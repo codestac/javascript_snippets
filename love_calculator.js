8b39af09faf6a852a5b8a4bc204a90e68a5022c7
Pabitra Raut <40185432+codestac@users.noreply.github.com>
2021-12-03 12:10:14 -0600

    Update and rename love_calculator.js to random_love_calculator.js

--- love_calculator.js
+++
@@ -1,7 +0,0 @@
-
-//psudo random number dice genrator
-
-var n = Math.random();
-n = n * 6
-n = Math.florr(n) + 1;
-console.log(n);
---
+++ random_love_calculator.js
@@ -0,0 +1,22 @@
+
+//psudo random number dice genrator
+
+var n = Math.random();
+n = n * 6
+n = Math.floor(n) + 1;
+console.log(n);
+
+//random love calculator
+
+prompt("What's your name?");
+prompt("What's their name?");
+
+var loveScore = math.random() * 100;
+loveScore = Math.floor(loveScore) + 1; //so zero cannot be a calculated percentage
+
+if (loveScore > 70) {
+  aleart("Your Love Score is" + loveScore + "%" + "You love each other");
+} else {
+  aleart("Your Love Score is" + loveScore + "%");
+}
+alert("Your love score is " +  loveScore + "%");
