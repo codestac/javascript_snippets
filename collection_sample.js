04959290e65728057444d10b7398fc5f7ab5f02a
Pabitra Raut <40185432+codestac@users.noreply.github.com>
2021-12-03 14:41:22 -0600

    Update collection_sample.js

--- collection_sample.js
+++ collection_sample.js
@@ -2,5 +2,8 @@ var guestList = ["Pabi", "Jim", "Paula", "Mamta"]
 console.log(guestList[0]);
 
 var guestName = prompt("Whats your name? ");
-guestList.includes(guestName) {
-  alert
+if (guestList.includes(guestName)) {
+  alert("Welcome"); 
+} else {
+  alert("Sorry, may be next time!");
+}
